d095b37d6c8dec99a10137d57aad7cd9791374d3
joshlee-7 <joshualee721@gmail.com>
2022-09-29 22:14:42 -0500

    added card styling

--- src/components/Coursecard.jsx
+++ src/components/Coursecard.jsx
@@ -1,8 +1,18 @@
 const Coursecard = ({ course }) => {
   return (
-    <p>
-      {course.term} CS {course.number}: {course.title}
-    </p>
+    <div className="card m-1 p-2">
+    
+    <div className="card-body">
+      <h5 className="card-title">{course.term} CS {course.number}</h5>
+      <p className="card-text">{course.title}</p>
+      </div>
+      <div className="align-bottom p-1">
+        <hr/>
+      <p className="card-text ">{course.meets}</p>
+      </div>
+    
+  </div>
+
   );
 };
 
--- src/components/Courselist.jsx
+++ src/components/Courselist.jsx
@@ -1,8 +1,9 @@
 import Coursecard from "./Coursecard";
+import './Courselist.css'
 
 const Courselist = ({ courses }) => {
   return (
-    <div>
+    <div className="course-list">
       {Object.entries(courses).map(([id, course]) => (
         <Coursecard key={id} course={course} />
       ))}
